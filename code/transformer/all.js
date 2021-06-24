b735b6c546b5a549a240976a25a5c3726d2a05e1
ChienChih <ana31005@gmail.com>
2021-06-24 17:21:48 +0800

    修改 transformer 頁面

--- code/transformer/all.js
+++ code/transformer/all.js
@@ -88,30 +88,158 @@ const data = [
     seal: '氣封式',
     oilPort: '法蘭(大)',
     oilOutlet: '法蘭(大)',
+},
+{
+    area: 'BSS1',
+    transformerName: 'TR 4001A',
+    transformerNumber: '865203',
+    transformerState: '正常',
+    mostRecentDate: '2012/12/28',
+    againDate: '2012/12/28',
+    manufacturer: '士林',
+    createDate: '1996/02',
+    kVA: '9000',
+    onec: '114',
+    twice: '0.3',
+    oil: '1000',
+    coil: '55',
+    seal: '氣封式',
+    oilPort: '法蘭(大)',
+    oilOutlet: '法蘭(大)',
+},
+{
+    area: 'BSS1',
+    transformerName: 'TR 4001A',
+    transformerNumber: '865203',
+    transformerState: '正常',
+    mostRecentDate: '2012/12/28',
+    againDate: '2012/12/28',
+    manufacturer: '士林',
+    createDate: '1996/02',
+    kVA: '9000',
+    onec: '114',
+    twice: '0.3',
+    oil: '1000',
+    coil: '55',
+    seal: '氣封式',
+    oilPort: '法蘭(大)',
+    oilOutlet: '法蘭(大)',
+},
+{
+    area: 'BSS1',
+    transformerName: 'TR 4001A',
+    transformerNumber: '865203',
+    transformerState: '正常',
+    mostRecentDate: '2012/12/28',
+    againDate: '2012/12/28',
+    manufacturer: '士林',
+    createDate: '1996/02',
+    kVA: '9000',
+    onec: '114',
+    twice: '0.3',
+    oil: '1000',
+    coil: '55',
+    seal: '氣封式',
+    oilPort: '法蘭(大)',
+    oilOutlet: '法蘭(大)',
+},
+{
+    area: 'BSS1',
+    transformerName: 'TR 4001A',
+    transformerNumber: '865203',
+    transformerState: '正常',
+    mostRecentDate: '2012/12/28',
+    againDate: '2012/12/28',
+    manufacturer: '士林',
+    createDate: '1996/02',
+    kVA: '9000',
+    onec: '114',
+    twice: '0.3',
+    oil: '1000',
+    coil: '55',
+    seal: '氣封式',
+    oilPort: '法蘭(大)',
+    oilOutlet: '法蘭(大)',
+},
+{
+    area: 'BSS1',
+    transformerName: 'TR 4001A',
+    transformerNumber: '865203',
+    transformerState: '正常',
+    mostRecentDate: '2012/12/28',
+    againDate: '2012/12/28',
+    manufacturer: '士林',
+    createDate: '1996/02',
+    kVA: '9000',
+    onec: '114',
+    twice: '0.3',
+    oil: '1000',
+    coil: '55',
+    seal: '氣封式',
+    oilPort: '法蘭(大)',
+    oilOutlet: '法蘭(大)',
+},
+{
+    area: 'BSS1',
+    transformerName: 'TR 4001A',
+    transformerNumber: '865203',
+    transformerState: '正常',
+    mostRecentDate: '2012/12/28',
+    againDate: '2012/12/28',
+    manufacturer: '士林',
+    createDate: '1996/02',
+    kVA: '9000',
+    onec: '114',
+    twice: '0.3',
+    oil: '1000',
+    coil: '55',
+    seal: '氣封式',
+    oilPort: '法蘭(大)',
+    oilOutlet: '法蘭(大)',
+},
+{
+    area: 'BSS1',
+    transformerName: 'TR 4001A',
+    transformerNumber: '865203',
+    transformerState: '正常',
+    mostRecentDate: '2012/12/28',
+    againDate: '2012/12/28',
+    manufacturer: '士林',
+    createDate: '1996/02',
+    kVA: '9000',
+    onec: '114',
+    twice: '0.3',
+    oil: '1000',
+    coil: '55',
+    seal: '氣封式',
+    oilPort: '法蘭(大)',
+    oilOutlet: '法蘭(大)',
 }]
 
 function tableRender() {
     let titleStr = '';
     let dataStr = '';
     titleStr += `
-      <tr style="color: white";>
-          <td>區域篩選</td>
-          <td>變壓器名稱</td>
-          <td>變壓器序號</td>
-          <td>變壓器狀況</td>
-          <td>最近取樣送試日期</td>
-          <td>建議再送試日期</td>
-          <td>製造廠商</td>
-          <td>製造日期</td>
-          <td>容量(kVA)</td>
-          <td>一次側</td>
-          <td>二次側</td>
-          <td>油量</td>
-          <td>線圈溫升</td>
-          <td>密封方式</td>
-          <td>進油口</td>
-          <td>出油口</td>
-      </tr>`
+    <thead>
+        <tr class="table-header";>
+            <td>區域篩選</td>
+            <td>變壓器序號</td>
+            <td>變壓器狀況</td>
+            <td>最近取樣送試日期</td>
+            <td>建議再送試日期</td>
+            <td>製造廠商</td>
+            <td>製造日期</td>
+            <td>容量(kVA)</td>
+            <td>一次側</td>
+            <td>二次側</td>
+            <td>油量</td>
+            <td>線圈溫升</td>
+            <td>密封方式</td>
+            <td>進油口</td>
+            <td>出油口</td>
+            <td>變壓器名稱</td>
+        </tr>
+    </thead>`
     data.forEach((res) => { 
     dataStr +=`
     <tr class="data-table">
@@ -123,7 +251,7 @@ function tableRender() {
         <td>${res.againDate}</td>
         <td>${res.manufacturer}</td>
         <td>${res.createDate}</td>
-        <td>${res.kVA})</td>
+        <td>${res.kVA}</td>
         <td>${res.onec}</td>
         <td>${res.twice}</td>
         <td>${res.oil}</td>
